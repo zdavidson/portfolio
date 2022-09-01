51e17dff52cc2ec26c51367c5c42bc8daa226426
Zoë Davidson <zoe.a.h.davidson@gmail.com>
2022-09-01 14:26:18 -0400

    update location

--- src/components/homepage/AboutMe.js
+++ src/components/homepage/AboutMe.js
@@ -20,17 +20,18 @@ const AboutMe = () => {
           </Row>
           <Row>
             <p>
-              I’m a software engineer looking for freelance work at a companies
-              that are looking to make a positive difference in the world. I
-              focus on writing accessible HTML, using modern CSS practices and
-              writing clean JavaScript. When it comes to front end technologies,
-              I'm well versed in React, but I can adapt to whatever tools are
+              I’m a software engineer looking for a position at a company that
+              is looking to make a positive difference in the world. I focus on
+              writing accessible HTML, using modern CSS practices and writing
+              clean JavaScript. When it comes to front end technologies, I'm
+              well versed in React, but I can adapt to whatever tools are
               required. As far as the back end goes, I have experience with
-              Node.js, Express, and PostgreSQL. I’m currently based in Toronto,
-              ON, but I’m happiest working remotely and have experience in
-              remote teams. When I’m not coding, you’ll find me taking photos
-              and travelling. I love art in all its forms, but particularly in
-              print or on film. I’d love for you to check out my work{" "}
+              Node.js, Express, and PostgreSQL. I’m currently based in
+              Washington, DC, but I’m happiest working remotely and have
+              experience being on remote teams. When I’m not coding, you’ll find
+              me taking photos and travelling. I love art in all its forms, but
+              particularly in print or on film. I’d love for you to check out my
+              work{" "}
               <a className="no-style" href="/portfolio">
                 here
               </a>
